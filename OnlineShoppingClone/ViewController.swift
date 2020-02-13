1b3dc084e4159d4ccf71ad087008369ac39bf23f
Nodirbek Asqarov <devasqarov@gmail.com>
2020-02-13 15:23:38 +0500

    Initial Commit

---
+++ OnlineShoppingClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnlineShoppingClone
+//
+//  Created by Nodirbek Asqarov on 2/13/20.
+//  Copyright © 2020 Nodirbek Asqarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
