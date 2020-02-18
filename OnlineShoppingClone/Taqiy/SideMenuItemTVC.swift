da1fef150d83e3797f3f955ee454ac2043fdf9ab
Nodirbek Asqarov <devasqarov@gmail.com>
2020-02-18 15:50:56 +0500

    Add Eldor, Rahmatillo, Shakhzod, Taqiy folders and its files

---
+++ OnlineShoppingClone/Taqiy/SideMenuItemTVC.swift
@@ -0,0 +1,28 @@
+//
+//  SideMenuItemTVC.swift
+//  OnlineShoppingClone
+//
+//  Created by Mukhammadyunus on 2/15/20.
+//  Copyright © 2020 Nodirbek Asqarov. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuItemTVC: UITableViewCell {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func setData() {
+        
+    }
+
+    
+}
